9105bebb6e918bfc139cad18625b08e66a8afe27
Besi <amulibesmira95@gmail.com>
2022-02-10 14:36:36 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,12 @@
 #include "main.h"
-/**
- * print_alphabet - prints the alphabet in lower care
- * followed by new line
- * Return: Always 0 (Success)
- */
-void print_alphabet(void)
-{
-	char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-		_putchar(ch);
-	_putchar('\n');
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
