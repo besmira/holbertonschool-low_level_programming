d42c36bd6ed939bebb8cb322284c9149507e6c2a
Besi <amulibesmira95@gmail.com>
2022-02-10 14:06:13 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,18 @@
+#include "main.h"
 
+/**
+  * print_alphabet - Make the alphabet
+  *
+  * Return: void
+  */
+void print_alphabet(void)
+{
+
+	int n;
+
+	for (n = 'a' ; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
