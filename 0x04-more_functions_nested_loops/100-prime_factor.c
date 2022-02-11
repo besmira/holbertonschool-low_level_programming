5bc0f8424139b2037eecd391fc0ba175e1db5104
Besi <amulibesmira95@gmail.com>
2022-02-11 14:15:43 +0100

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long num = 612852475143;
+	long division = 2;
+	long prime = 0;
 
+	while (num != 1)
+	{
+		if (num % division == 0)
+		{
+			num = num / division;
+			prime = division;
+		}
+		division += 1;
+	}
+	printf("%ld\n", prime);
+	return (0);
+}
