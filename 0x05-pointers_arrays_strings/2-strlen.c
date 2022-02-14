3eb476bfae8e7f8efff1ec4598bcaa6246596995
Besi <amulibesmira95@gmail.com>
2022-02-14 13:57:30 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,15 @@
+#include "main.h"
 
+/**
+ * swap_int - swaps the value of two integers
+ *@a: input
+ *@b: input
+ */
+void swap_int(int *a, int *b)
+{
+int i, j;
+i = *a;
+j = *b;
+*a = j;
+*b = i;
+}
