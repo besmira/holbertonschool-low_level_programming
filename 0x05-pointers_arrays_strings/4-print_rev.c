fec82006baa505a6962627e965f35986140efda1
Besi <amulibesmira95@gmail.com>
2022-02-14 14:53:09 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,19 @@
+#include "main.h"
 
+/**
+ * print_rev - prints string reversed
+ * @s: input
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+int i, k;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+
+for (k = i - 1; k >= 0; k--)
+_putchar(s[k]);
+_putchar('\n');
+}
