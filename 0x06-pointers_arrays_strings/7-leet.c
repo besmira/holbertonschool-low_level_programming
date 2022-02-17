3b345308d43b8fe4cc5db0da6e24a0d7aa5938d0
Besi <amulibesmira95@gmail.com>
2022-02-17 14:16:03 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,24 @@
+#include "main.h"
 
+/**
+ * leet - encodes a string into 1337
+ *@s: character
+ * Return: string.
+ */
+
+char *leet(char *s)
+{
+	int a[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int b[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+				s[i] = b[j];
+		}
+	}
+	return (s);
+}
