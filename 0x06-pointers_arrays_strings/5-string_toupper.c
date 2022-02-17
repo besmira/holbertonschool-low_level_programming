e11a862d057a38a0a1b18d2fa2d5ba2a24bf4e70
Besi <amulibesmira95@gmail.com>
2022-02-17 13:34:11 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,23 @@
+#include "main.h"
 
+/**
+ * string_toupper - change all lowercase to upper
+ *
+ * @a: input
+ * Return: a
+ */
+char *string_toupper(char *a)
+{
+int i, x;
+
+for (i = 0; a[i] != '\0'; i++)
+{
+x = a[i];
+if ((x >= 'a') && (x <= 'z'))
+{
+x = x - 32;
+a[i] = x;
+}
+}
+return (a);
+}
