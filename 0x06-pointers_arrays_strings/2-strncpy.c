aee0e7d7ff3037339bdfd6075283245aefee474c
Besi <amulibesmira95@gmail.com>
2022-02-17 10:51:55 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,4 +7,17 @@
  * Return: return the copied string in dest
  */
 char *_strncpy(char *dest, char *src, int n)
-2-strncpy.c
+{
+    int i = 0 
+      while (src[i] != '\0' && i < n)
+	{
+	  	dest[i] = src[i];
+	   	i++;
+	}
+	   while (i < n)
+	{
+		 dest[i] = '\0';
+		 i++;
+	}
+	return (dest);
+}
