92ecb68491f1e46423abd07424960ad35c4c82ca
Besi <amulibesmira95@gmail.com>
2022-02-17 10:43:04 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,3 +7,19 @@
  * Return: return a pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	return (dest);
+}
