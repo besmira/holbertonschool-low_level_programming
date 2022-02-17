c631816c737a8d4dd3aff5cbd0db81a8d1699bf5
Besi <amulibesmira95@gmail.com>
2022-02-17 11:37:36 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,9 +11,9 @@ int _strcmp(char *s1, char *s2)
     int n1 = 0
      while (s1[i] != '\0' || s2[i] != '\0')
      {
-      if (s1[i] = s2[i])
-      n1 = s1[i] - s2[i];
-                break;
+     	 if (s1[i] = s2[i])
+         n1 = s1[i] - s2[i];
+   	             break;
       else
                 n1 = 0;
       i++;
