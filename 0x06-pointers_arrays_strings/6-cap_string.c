408ec5714ffaf16cca90797fd35533d4b1808956
Besi <amulibesmira95@gmail.com>
2022-02-17 13:42:56 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,36 @@
+#include "main.h"
 
+/**
+ * cap_string - capitalise all words of a string
+ *@s: string
+ * Return: Always 0.
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] = s[0] - 'a' + 'A';
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'A' && s[i] <= 'Z')
+			s[i] = s[i];
+		else if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+		{
+			if (s[i] == '"' || s[i] == '(' || s[i] == ')' ||
+					s[i] == '{' || s[i] == '}' ||
+					s[i] == '\n' || s[i] == '\t' ||
+					s[i] == '.' || s[i] == '?' ||
+					s[i] == ';' || s[i] == '!' ||
+					s[i] == ',' || s[i] == ' ' ||
+					s[i] == '.')
+			{
+				s[i + 1] = s[i + 1] - 'a' + 'A';
+			}
+		}
+		i++;
+	}
+	return (s);
+
+}
