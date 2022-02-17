c80f63875cc9a28923766f22a6c862c4127237a1
Besi <amulibesmira95@gmail.com>
2022-02-17 10:49:37 +0100

    Create 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ * Return: return the copied string in dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+2-strncpy.c
