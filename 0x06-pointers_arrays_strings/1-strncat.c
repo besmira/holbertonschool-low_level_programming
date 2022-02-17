09a312d1c3456f710b86853128872c8f0deb3705
Besi <amulibesmira95@gmail.com>
2022-02-17 10:30:54 +0100

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ * Return: return a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
