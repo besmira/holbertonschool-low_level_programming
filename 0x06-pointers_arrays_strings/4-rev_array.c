c4dde6f117bc5ee91bde1a5fbb3a202b2033faf3
Besi <amulibesmira95@gmail.com>
2022-02-17 13:20:45 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,22 @@
+#include "main.h"
 
+/**
+ * reverse_array - reverses array
+ * @a: input
+ *@n: input
+ *
+ */
+void reverse_array(int *a, int n)
+{
+int arr;
+int  i, k;
+i = 0;
+
+for (k = n - 1; k > x; k--)
+{
+arr = a[i];
+a[i] = a[k];
+a[k] = arr;
+i++;
+}
+}
