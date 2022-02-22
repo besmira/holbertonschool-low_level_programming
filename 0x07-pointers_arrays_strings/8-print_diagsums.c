32e5e75041ef436b4170593478879d216f17d0d8
Besi <amulibesmira95@gmail.com>
2022-02-22 14:50:09 +0100

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,28 +10,29 @@
   */
 void print_diagsums(int *a, int size)
 {
-  int matrix[3][3] = {{1, 2, 3},{4, 5, 6},{7, 8, 9}};
+  int matrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
   int sum = 0;
   for (int i = 0; i < 3; i++)
   {
     for (int j = 0; j < 3; j++)
     {
-      if(i == j) 
+      if (i == j)
       {
        sum += matrix[i][j];
       }
     }
   }
   printf("Matrix = \n");
-  for(int i = 0; i < 3; i++)
+  for (int i = 0; i < 3; i++)
   {
-    for(int j = 0; j < 3; j++)
+    for (int j = 0; j < 3; j++)
     {
       printf("%d ", matrix[i][j]);
     }
-    printf("\n"); // new line
+    printf("\n");
   }
   printf("Sum of diagonal elements = %d\n", sum);
-
+ {
   return 0;
+ }
 }
