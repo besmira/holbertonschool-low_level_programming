7b99b9ca4c3dbd3461e0fad3559c986fb1294bdf
Besi <amulibesmira95@gmail.com>
2022-02-23 00:00:23 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,1 +1,9 @@
-
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string
+ * @accept: bytes
+ * Returns the number of bytes in the initial segment of s 
+ * whith only of bytes from accept
+ */
+ 
