899bcb204a9dc0c6afaafa7ed9cfccb4f71c4d34
Besi <amulibesmira95@gmail.com>
2022-02-22 14:54:00 +0100

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,7 +13,6 @@ void print_diagsums(int *a, int size)
 int matrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 int sum = 0;
   for (int i = 0; i < 3; i++)
- 
   {
     for (int j = 0; j < 3; j++)
     {
