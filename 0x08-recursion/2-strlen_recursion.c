cd87bd246ca4a81af7b764db405cd9183a0dbca3
Besi <amulibesmira95@gmail.com>
2022-02-23 13:17:23 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,13 +8,11 @@
 
 int _strlen_recursion(char *s)
 {
-	int a;
 
-	a = 0;
 	if (*s == '\0')
 		return (0);
 
-	a = 1 +  _strlen_recursion(s + 1);
+	c = 1 +  _strlen_recursion(s + 1);
 
 	return (c);
 }
