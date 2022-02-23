5f4f46aaa5f0fc69598da574d69d22cd507aac38
Besi <amulibesmira95@gmail.com>
2022-02-23 11:50:01 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,12 +5,11 @@
  * @s: string
  */
 
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
-
-	if (*s ! = = '\0');
-
+	if (*s)
 	{
 		_print_rev_recursion(s + 1);
-                _putchar(*s);
+		_putchar(*s);
+	}
 }
