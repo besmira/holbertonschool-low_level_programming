46400a13f5f65f7b62bf16d9f8873ab138d71fa4
Besi <amulibesmira95@gmail.com>
2022-03-01 14:57:29 +0100

    Update 0-whatsmyname.c

--- 0x0A. C - argc, argv/0-whatsmyname.c
+++ 0x0A. C - argc, argv/0-whatsmyname.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ * @argc: Argument
+ * @argv: Argument
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
